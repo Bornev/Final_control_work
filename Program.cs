c721eaae55e20a2f8e4a2f3c030f56d745bc210e
Nikolai Borodulin <bornev-86@mail.ru>
2024-04-13 21:24:40 +0500

    code writing

--- Program.cs
+++ Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод исходного массива строк
+        Console.WriteLine("Введите строки через пробел:");
+        string[] array = Console.ReadLine()!.Split(' ');
+
+        int size = array.Length;
+        int index = 0;
+        int count = 0;
+
+        // Подсчет количества строк с длиной <= 3
+        for (index = 0; index < size; index++)
+        {
+            if (array[index].Length <= 3)
+            {
+                count++;
+            }
+        }
+
+        // Создание нового массива для строк с длиной <= 3
+        string[] arrayNew = new string[count];
+        int IndexArrayNew = 0;
+
+        // Заполнение нового массива строками с длиной <= 3
+        for (index = 0; index < size; index++)
+        {
+            if (array[index].Length <= 3)
+            {
+                arrayNew[IndexArrayNew] = array[index];
+                IndexArrayNew++;
+            }
+        }
+
+        // Вывод результата
+        Console.Write("Строки с длиной <= 3: [");
+        for (int i = 0; i < arrayNew.Length; i++)
+        {
+            Console.Write($"\"{arrayNew[i]}\"");
+            if (i < arrayNew.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+
+    }
+}
+
